a911f95871a11241cc9ff8d73ae5c0a784a76ff9
Manish <chemanishazad@gmail.com>
2024-06-08 14:04:20 +0530

    final

---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+import 'package:u_credit_card/u_credit_card.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  bool isLightTheme = false;
+  String bankName = '';
+
+  final TextEditingController _bankName = TextEditingController();
+
+  final OutlineInputBorder border = OutlineInputBorder(
+    borderSide: BorderSide(
+      color: Colors.grey.withOpacity(0.7),
+      width: 2.0,
+    ),
+  );
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      isLightTheme ? SystemUiOverlayStyle.dark : SystemUiOverlayStyle.light,
+    );
+    return MaterialApp(
+      title: 'Flutter Credit Card View Demo',
+      debugShowCheckedModeBanner: false,
+      themeMode: isLightTheme ? ThemeMode.light : ThemeMode.dark,
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleMedium: TextStyle(color: Colors.black, fontSize: 18),
+        ),
+        colorScheme: ColorScheme.fromSeed(
+          brightness: Brightness.light,
+          seedColor: Colors.white,
+          background: Colors.black,
+          primary: Colors.black,
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          hintStyle: const TextStyle(color: Colors.black),
+          labelStyle: const TextStyle(color: Colors.black),
+          focusedBorder: border,
+          enabledBorder: border,
+        ),
+      ),
+      darkTheme: ThemeData(
+        textTheme: const TextTheme(
+          titleMedium: TextStyle(color: Colors.white, fontSize: 18),
+        ),
+        colorScheme: ColorScheme.fromSeed(
+          brightness: Brightness.dark,
+          seedColor: Colors.black,
+          background: Colors.white,
+          primary: Colors.white,
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          hintStyle: const TextStyle(color: Colors.white),
+          labelStyle: const TextStyle(color: Colors.white),
+          focusedBorder: border,
+          enabledBorder: border,
+        ),
+      ),
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Builder(
+          builder: (BuildContext context) {
+            return Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: ExactAssetImage(
+                    isLightTheme ? 'assets/bg-light.png' : 'assets/bg-dark.png',
+                  ),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              child: SafeArea(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    IconButton(
+                      onPressed: () => setState(() {
+                        isLightTheme = !isLightTheme;
+                      }),
+                      icon: Icon(
+                        isLightTheme ? Icons.light_mode : Icons.dark_mode,
+                      ),
+                    ),
+                    Center(
+                      child: CreditCardUi(
+                        cardHolderFullName: _bankName.text,
+                        cardNumber: '1234567812345678',
+                        validFrom: '01/23',
+                        validThru: '01/28',
+                        topLeftColor: Colors.pink,
+                        doesSupportNfc: true,
+                        placeNfcIconAtTheEnd: true,
+                        cardProviderLogo: FlutterLogo(),
+                        autoHideBalance: true,
+                        enableFlipping: true,
+                        cvvNumber: '123',
+                      ),
+                    ),
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: <Widget>[
+                            const SizedBox(height: 20),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 16, right: 16.0),
+                              child: TextFormField(
+                                controller: _bankName,
+                                onChanged: (value) {
+                                  setState(() {
+                                    bankName = value;
+                                  });
+                                },
+                                decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  labelText: 'Holder Name',
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
